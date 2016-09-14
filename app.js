341e63176449219882316fc2aa54a50b32721c14
wpressdev2 <wpressdev2@gmail.com>
2016-09-14 13:05:17 +0200

    ss

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 var http = require('http'),
     express = require("express"),
-    favicon = require("serve-favicon"),
-    mysql = require("mysql"),
     logger = require("morgan"),
     cookieParser = require("cookie-parser"),
-    bodyParser = require("body-parser"),
+    bodyParser = require("body-parser")
     passwordHash = require('password-hash'),
     passport = require('passport'),
     LocalStrategy = require('passport-local').Strategy,
     expressSession = require('express-session'),
-    md5 = require('MD5'),
     dateFormat = require("dateformat"),
     now = new Date(),
     nodemailer = require('nodemailer'),
     smtpTransport = require('nodemailer-smtp-transport'),
     flash = require('connect-flash');
-
+    
 var environment = process.env.ENVIRONMENT || "production";
 
 var routes = require('./routes/index');
@@ -56,61 +53,6 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 
-// Submitting feedback
-app.post("/", function (req, res) {
-    var feedback_date = dateFormat(now, "yyyy-mm-dd hh:MM:ss");
-    var emailBody = 'Hello, <br><br> A new user has given the feedback. Following are the feedback information:<br><br><br>' +
-                    'Name: ' + req.body.firstname+ ' ' +req.body.lastname + 
-                    '<br><br> Email: ' + req.body.email + 
-                    '<br><br> Options: ' + req.body.options + 
-                    '<br><br> Satisfaction Level: ' + req.body.satisfaction + 
-                    '<br><br> Comments: <br>' + req.body.comments + 
-                    '<br><br> Date: ' + feedback_date + '<br><br><br><br>' +
-                    '<br> Regards,<br><br>Admin<br>';
-
-    objConn.query("INSERT INTO feedback (firstName,lastName,email,options,satisfaction_level,comments,feedback_date) VALUES (?,?,?,?,?,?,?)", [req.body.firstname,req.body.lastname,req.body.email,req.body.options,req.body.satisfaction,req.body.comments,feedback_date], function (err, content, fields) {
-            if(err){
-                if(err.code !== "ER_DUP_ENTRY"){
-                    console.log(err);
-                }
-            }else{
-                console.log(emailBody);
-                // create reusable transporter object using the default SMTP transport
-                var options = {
-                    host: 'smtp.gmail.com', // hostname 
-                    secureConnection: true, // use SSL    
-                    service: 'gmail',
-                    auth: {
-                        user: AUTH_USERNAME,
-                        pass: AUTH_PASSWORD
-                    }
-                  };
-                  var transporter = nodemailer.createTransport(smtpTransport(options));
-
-                  // setup e-mail data with unicode symbols
-                var mailOptions = {
-                    from: req.body.email,
-                    to: TO_EMAIL,
-                    subject: 'User feedback',
-                    html: emailBody
-                };
-
-                //var msg = 'Thank you for your feedback';
-                // send mail with defined transport object
-                transporter.sendMail(mailOptions, function(error, res){
-                    if(error){
-                        console.log(error);
-                    }else{
-                        console.log("Message sent: " + msg);
-                    }
-                    //smtpTransport.close(); // shut down the connection pool, no more messages
-                });
-                //logger.info("Thank you for your feedback!");
-                res.redirect("/");
-            }
-        });
-    });
-    
 app.get('/', function(req, res) {
   response.render('index');
 });
