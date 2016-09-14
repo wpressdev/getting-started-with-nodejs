35fbb03169136f06ca96157d9c5a1c73a325a1d1
wpressdev2 <wpressdev2@gmail.com>
2016-09-14 10:21:35 +0200

    H

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.render('home');
 });
 
 app.get('/cool', function(request, response) {
