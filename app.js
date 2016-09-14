f07a065d2d42fee3fcb1c89f123b3fc5337c81d1
wpressdev2 <wpressdev2@gmail.com>
2016-09-14 13:15:33 +0200

    rewr

--- app.js
+++ app.js
@@ -1,23 +1,15 @@
 var express = require('express');
-
-var routes = require('./routes/index');
-var signin = require('./routes/signin');
-var feedbacks = require('./routes/feedbacks');
-
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use('/', routes);
-app.use('/signin', signin);
-app.use('/feedbacks', feedbacks);
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
-app.get('/', function(request, response) {
+app.get('/', function(req, res) {
   response.render('index');
 });
 
