283c71424fc45e1d6d8de489372dcd3d06e37846
wpressdev2 <wpressdev2@gmail.com>
2016-09-14 13:12:57 +0200

    ffdsf

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 var express = require('express');
+
+var routes = require('./routes/index');
+var signin = require('./routes/signin');
+var feedbacks = require('./routes/feedbacks');
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use('/', routes);
+app.use('/signin', signin);
+app.use('/feedbacks', feedbacks);
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
